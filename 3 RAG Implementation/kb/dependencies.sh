67a79a136bb9a4107c99f5d498fd7db21d71e5e4
BruceTraining <b@btauthors.com>
2025-11-15 21:28:38 +0100

    Create dependencies.sh

---
+++ 3 RAG Implementation/kb/dependencies.sh
@@ -0,0 +1,7 @@
+pip install langchain \
+             langchain-community \
+             langchain-openai \
+             pypdf \
+             python-docx \
+             chromadb \
+             python-dotenv
